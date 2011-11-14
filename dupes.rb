483541da027bef2d56d0862e1017274decabb8c1
Jeff Escalante <jeff.escalante@carrotcreative.com>
2011-11-14 13:16:54 -0500

    initial commit - added script

---
+++ dupes.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/ruby
+require 'digest/md5'
+require 'fileutils'
+
+list = []
+files = []
+files << Dir['**/*.*']
+files.first.each do |f|
+  checksum = Digest::MD5.hexdigest(File.read(f))
+  if list.include?(checksum)
+    FileUtils.mv(f, "/Users/jeffescalante/.Trash")
+    puts "duplicate at #{f} detected, moved to trash"
+  else
+    list << checksum
+  end
+end
+puts "done!"
